9f814dd3e5362de3dddae26f3348733b8fc7a581
Christian Becher <phaziz@gmail.com>
2015-07-31 12:23:47 +0200

    THEMES Update

--- THEMES/index.php
+++ THEMES/index.php
@@ -9,19 +9,20 @@
 <meta name="description" content="{{@ CONSTRUCTR_PAGE_DESCRIPTION @}}">
 <link rel="stylesheet" href="{{@ CONSTRUCTR_BASE_URL @}}/CONSTRUCTR-CMS/ASSETS/materialize/css/materialize.min.css">
 <style>
-div#menu ul ul {display: none;z-index:999;}
-div#menu ul li:hover > ul {display: block;z-index:999;}
-div#menu ul {padding: 0 10px;  list-style: none;position: relative;display: inline-table;}
-div#menu ul:after {content: ""; clear: both; display: block;}
-div#menu ul li {float: left;}
-div#menu ul li:hover {background: #4b545f;background: linear-gradient(top, #4f5964 0%, #5f6975 40%);background: -moz-linear-gradient(top, #4f5964 0%, #5f6975 40%);background: -webkit-linear-gradient(top, #4f5964 0%,#5f6975 40%);}
-div#menu ul li:hover a {color: #fff;}
-div#menu ul li a {display: block; padding: 25px 40px;color: #757575; text-decoration: none;}
-div#menu ul ul {background: #5f6975; border-radius: 0px; padding: 0;position: absolute; top: 100%;}
-div#menu ul ul li {float: none;position: relative;}
-div#menu ul ul li a {padding: 15px 40px;color: #fff;}	
-div#menu ul ul li a:hover {background: #4b545f;}
-div#menu ul ul ul {position: absolute; left: 100%; top:0;}
+div#menu{background: #000;margin:0 0 25px 0;padding:0 0 0 0}
+div#menu ul ul {display: none;z-index:999;margin:0 0 0 0;padding:0 0 0 0}
+div#menu ul li:hover > ul {display: block;z-index:999}
+div#menu ul {padding-left: 10px;  list-style: none;position: relative;display: inline-table}
+div#menu ul:after {content:""; clear: both; display: block}
+div#menu ul li {float: left;width:150px}
+div#menu ul li:hover {background: #ff0035}
+div#menu ul li:hover a {color: #fff}
+div#menu ul li a {display: block; padding: 10px 10px;color: #fff; text-decoration: none;text-align:center}
+div#menu ul ul {background: #ff0035; padding: 0;position: absolute; top: 100%}
+div#menu ul ul li {float: none;position: relative}
+div#menu ul ul li a {padding: 10px 10px;color: #fff;border-top: 1px solid #fff;text-align:left}
+div#menu ul ul li a:hover {background: #ff0035}
+div#menu ul ul ul {position: absolute; left: 100%; top:0}
 {{@ PAGE_CSS @}}
 </style>
 </head>
